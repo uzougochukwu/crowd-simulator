fa7644254ee213987b5c34533d6f262b2154401d
Uzo Ugochukwu <uugochukwu@aol.com>
2025-03-31 20:43:37 +0100

    added error handling and syscall definitions to main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,8 @@
 	;; might not need mmap syscall, might be better to simply modify stack memory, then write that memory to the framebuffer
 
 	default rel
+%include "/home/calebmox/crowd-simulator/src/graphical-output/library/system/syscalls.asm"
+%include "/home/calebmox/crowd-simulator/src/graphical-output/library/system/error_handling.asm"
 	
 	section .rodata
 
@@ -26,7 +28,6 @@ injurylength:	equ $-injury
 
 	section .bss
 	answer resq 8
-	answer2 resb 1
 
 
 	section .text
@@ -35,20 +36,41 @@ injurylength:	equ $-injury
 _start:	
 
 
-	mov rax, 0x1
+	mov rax, __NR_write
 	mov rdi, 0x1
 	mov rsi, Intro
 	mov rdx, IntroLength
 	syscall
 
+	cmp rax, 0x0
+	jge intro_input
+
+	lea rcx, [_start]	; load address of _start label to rcx
+
+	mov qword [instruction_pointer], rcx ; move the address of the start label to the instruction_pointer memory location, in the error handling file
+
+	call error_handling
+
 intro_input:
 
 	
-	mov rax, 0x0
+	mov rax, __NR_read
 	mov rdi, 0x0
 	mov rsi, answer
 	mov rdx, 2
 	syscall
+
+	cmp rax, 0x0
+	jge main_next
+
+	lea rcx, [intro_input]	; load address of _start label to rcx
+
+	mov qword [instruction_pointer], rcx ; move the address of the start label to the instruction_pointer memory location, in the error handling file
+
+	call error_handling
+
+main_next:	
+
 	
 	mov al, [answer]
 
@@ -56,21 +78,41 @@ intro_input:
 	cmp rax, 0x79
 	je death_script
 	
-	mov rax, 0x1
+	mov rax, __NR_write
 	mov rdi, 0x1
 	mov rsi, injury
 	mov rdx, injurylength
 	syscall
 
+	cmp rax, 0x0
+	jge cover_up_decision
+
+	lea rcx, [main_next]	; load address of _start label to rcx
+
+	mov qword [instruction_pointer], rcx ; move the address of the start label to the instruction_pointer memory location, in the error handling file
+
+	call error_handling
+
 
 
 cover_up_decision:
 
-	mov rax, 0x0
+	mov rax, __NR_read
 	mov rdi, 0x0
 	mov rsi, answer
 	mov rdx, 2
 	syscall
+
+	cmp rax, 0x0
+	jge main_second
+
+	lea rcx, [cover_up_decision]	; load address of relevant label to rcx
+
+	mov qword [instruction_pointer], rcx ; move the address of the start label to the instruction_pointer memory location, in the error handling file
+
+	call error_handling
+	
+main_second:	
 	
 	mov al, [answer]
 	cmp rax, 0x79
@@ -79,29 +121,60 @@ cover_up_decision:
 
 death_script:
 
-	mov rax, 0x1
+	mov rax, __NR_write
 	mov rdi, 0x1
 	mov rsi, death
 	mov rdx, deathlength
 	syscall
+
+	cmp rax, 0x0
+	jge decision_next
+
+	lea rcx, [death_script]	; load address of relevant label to rcx
+
+	mov qword [instruction_pointer], rcx ; move the address of the relevant label to the instruction_pointer memory location, in the error handling file
+
+	call error_handling
+
+decision_next:	
 	jmp cover_up_decision
 
 no_cover_up:
 
-	mov rax, 0x1
+	mov rax, __NR_write
 	mov rdi, 0x1
 	mov rsi, no_conspiracy
 	mov rdx, no_conspiracylength
 	syscall
+
+	cmp rax, 0x0
+	jge cover_up_next
+
+	lea rcx, [no_cover_up]	; load address of relevant label to rcx
+
+	mov qword [instruction_pointer], rcx ; move the address of the relevant label to the instruction_pointer memory location, in the error handling file
+
+	call error_handling
+	
+cover_up_next:	
 	jmp exit
 
 cover_up:	
 
-	mov rax, 0x1
+	mov rax, __NR_write
 	mov rdi, 0x1
 	mov rsi, conspiracy
 	mov rdx, conspiracylength
 	syscall
+
+	cmp rax, 0x0
+	jge exit
+
+	lea rcx, [cover_up]	; load address of _start label to rcx
+
+	mov qword [instruction_pointer], rcx ; move the address of the start label to the instruction_pointer memory location, in the error handling file
+
+	call error_handling
 
 exit:	
 	mov rax, 0x3c
